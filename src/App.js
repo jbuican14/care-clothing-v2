0c1f4c79cc6be0681069197de32285a0f0335388
Bew <Bew@sysops-MBP.broadband>
2020-12-21 15:27:17 +0000

    Add Header and style class (transition and animation using ::after

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import HomePage from 'pages/homepage/Homepage.component';
-import 'pages/homepage/Homepage.styles.scss';
 import ShopPage from 'pages/shop/shop.component';
+import Header from 'components/header/header.component';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
