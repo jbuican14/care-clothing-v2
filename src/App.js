0f57336e08f63ebe2622830275657d474af789b0
Bew <Bew@sysops-MBP.broadband>
2020-12-22 17:43:33 +0000

    Adding database on Firebase and adding check functionality to create a new user on the database if the log in from Google Account doesn't exist in the database

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import HomePage from 'pages/homepage/Homepage.component';
 import ShopPage from 'pages/shop/shop.component';
 import Header from 'components/header/header.component';
 import SignInAndSignUpPage from 'pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from 'firebase/firebase.utils';
+import { auth, createUserProfileDocument } from 'firebase/firebase.utils';
 
 class App extends React.Component {
   constructor() {
@@ -21,9 +21,10 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
-
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (user) => {
+      // this.setState({ currentUser: user });
+      await createUserProfileDocument(user);
+      // const userRef = await createUserProfileDocument(user);
       console.log(user);
     });
   }
