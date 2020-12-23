ab1565881efae2ae0358b95f4d381b4160368bad
Bew <Bew@sysops-MBP.broadband>
2020-12-23 20:53:28 +0000

    Fix smaill typos

--- src/components/cart-dropdown/cart-dropdown.compoenent.jsx
+++ src/components/cart-dropdown/cart-dropdown.compoenent.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
 
+import { connect } from 'react-redux';
+
 import CustomButton from 'components/custom-button/custom-button.component';
+import CartItem from 'components/cart-item/cart-item.component';
 
 import 'components/cart-dropdown/cart-dropdown.styles.scss';
 
-const CartDropdown = () => (
-  <div className="cart-dropdown">
-    <div className="cart-items">
-      <CustomButton>Go to Checkout</CustomButton>
+const CartDropdown = ({ cartItems }) => {
+  console.log('carttems', cartItems);
+  return (
+    <div className="cart-dropdown">
+      <div className="cart-items">
+        {cartItems.map((cartItem) => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))}
+        <CustomButton>Go to Checkout</CustomButton>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
-export default CartDropdown;
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems,
+});
+
+export default connect(mapStateToProps)(CartDropdown);
