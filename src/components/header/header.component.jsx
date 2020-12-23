692b7ec3fbf89a5b3debd18e6e782452ba3b9f00
Bew <Bew@sysops-MBP.broadband>
2020-12-23 13:37:18 +0000

    Add the toggleHidden to the header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -9,7 +9,7 @@ import CartDropdown from 'components/cart-dropdown/cart-dropdown.compoenent';
 
 import 'components/header/header.styles.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className="header">
     <Link to="/" className="logo-container">
       <Logo className="logo" />
@@ -34,12 +34,13 @@ const Header = ({ currentUser }) => (
 
       <CartIcon />
     </div>
-    <CartDropdown />
+    {hidden ? null : <CartDropdown />}
   </div>
 );
 
-const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser,
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden,
 });
 
 export default connect(mapStateToProps)(Header);
