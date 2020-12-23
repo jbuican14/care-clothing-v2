aeff140e24916fcd9c458aafdfa898d409b47f44
Bew <Bew@sysops-MBP.broadband>
2020-12-23 15:42:17 +0000

    Dispatch the add item cart to our collection item component

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,24 +1,36 @@
 import React from 'react';
 
+import { connect } from 'react-redux';
+import { addItem } from 'redux/cart/cart.action';
+
 import CustomButton from 'components/custom-button/custom-button.component';
 
 import 'components/collection-item/collection-item.styles.scss';
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
-  <div className="collection-item">
-    <div
-      className="image"
-      style={{
-        backgroundImage: `url(${imageUrl})`,
-      }}
-    />
+const CollectionItem = ({ item, addItem }) => {
+  const { name, price, imageUrl } = item;
+  return (
+    <div className="collection-item">
+      <div
+        className="image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
 
-    <div className="collection-footer">
-      <span className="name">{name}</span>
-      <span className="price">{price}</span>
+      <div className="collection-footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
+      </div>
+      <CustomButton inverted onClick={() => addItem(item)}>
+        Add to Cart
+      </CustomButton>
     </div>
-    <CustomButton inverted>Add to Cart</CustomButton>
-  </div>
-);
+  );
+};
 
-export default CollectionItem;
+const mapDispatchToProp = (dispatch) => ({
+  addItem: (item) => dispatch(addItem(item)),
+});
+
+export default connect(null, mapDispatchToProp)(CollectionItem);
