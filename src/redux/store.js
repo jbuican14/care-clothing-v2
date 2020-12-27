8a6199a3c4041f2813ba56d9121a243af2460315
Bew <Bew@sysops-MBP.broadband>
2020-12-27 18:16:30 +0000

    Remove logger from the production

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,7 +5,6 @@ import { persistStore } from 'redux-persist';
 import rootReducer from 'redux/root-reducer';
 
 const middlewares = [];
-
 if (process.env.NODE_ENV === 'development') {
   middlewares.push(logger);
 }
